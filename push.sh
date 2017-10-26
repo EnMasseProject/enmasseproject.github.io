9c07995d0e523ce9bf863b3b8acdf2947526ec93
Ulf Lilleengen <lulf@redhat.com>
2017-10-26 23:40:18 +0200

    Use deploy_key

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/EnMasseProject/enmasseproject.github.io.git > /dev/null 2>&1
+  git remote add origin-pages git@github.com:EnMasseProject/enmasseproject.github.io.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages master
 }
 
