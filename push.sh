5b0002064157baa27572a5e0567817334935b26c
Ulf Lilleengen <lulf@redhat.com>
2017-10-26 22:48:46 +0200

    Add travis files

---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add documentation
+  git add _include/documentation
+  git commit -a --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/EnMasseProject/enmasseproject.github.io.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages master
+}
+
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    setup_git
+    commit_website_files
+    upload_files
+fi
