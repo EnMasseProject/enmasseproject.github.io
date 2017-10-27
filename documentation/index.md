130ad752ab25a3d46f3482351e80d1b90c478e62
EnMasse CI <enmasse-ci@redhat.com>
2017-10-27 08:31:01 +0000

    Travis #6

--- documentation/index.md
+++
@@ -1,9 +0,0 @@
----
-layout: page
----
-# Documentation
-
-Welcome to the EnMasse documentation. Select the version of EnMasse you are using.
-
-{% assign menu = site.menus.docs %}
-{% include doc_menu.html %}
--- push.sh
+++ push.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-set -x
-
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
-commit_website_files() {
-  git checkout master
-  git add documentation/
-  git add _includes/documentation/
-  git add _data
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-    git remote add sshorigin git@github.com:EnMasseProject/enmasseproject.github.io.git
-#  git push -u origin-pages master
-    git push -u sshorigin master
-}
-
-setup_git
-commit_website_files
-upload_files
+cd _site
+git config user.email "enmasse-ci@redhat.com"
+git config user.name "EnMasse CI"
+git add --all
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --force origin master
