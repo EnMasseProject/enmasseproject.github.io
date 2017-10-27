8ae8d2e0258aa09e1e46f1ece8db07981b8e3b4d
EnMasse CI <enmasse-ci@redhat.com>
2017-10-27 09:39:27 +0000

    Travis #9

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd _site
-git config user.email "enmasse-ci@redhat.com"
-git config user.name "EnMasse CI"
-git add --all
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --force origin master
